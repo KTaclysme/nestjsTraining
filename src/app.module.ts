d01bb3f8606712fe7f8b94f65b1c32b77777ef60
Kevin TA <fr159004@MacBook-Pro.local>
2024-03-08 09:17:25 +0100

    linter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingMiddleware } from './logging/logging.middleware';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksController } from './tasks/tasks.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost:27017')],
-  controllers: [AppController, TasksController],
+  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
